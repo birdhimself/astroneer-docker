04f916e37e26f1d2db01d7780bc76db7c63dda8c
birdhimself <git@wuul.eu>
2025-07-19 10:14:20 +0200

    Update GitHub username references

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ python3 AstroTuxLauncher.py install
 
 if [[ "$DISABLE_ENCRYPTION" =~ ^([Tt][Rr][Uu][Ee]|1|[Yy][Ee][Ss])$ ]]; then
   echo Encryption will be disabled because DISABLE_ENCRYPTION is set.
-  echo Check https://github.com/C0Nd3Mnd/astroneer-docker?tab=readme-ov-file#configuring-clients for instructions on how to enable clients to connect to servers with encryption disabled.
+  echo Check https://github.com/birdhimself/astroneer-docker?tab=readme-ov-file#configuring-clients for instructions on how to enable clients to connect to servers with encryption disabled.
   sed -i 's/^DisableEncryption.*/DisableEncryption = true/' launcher.toml
 else
   echo Encryption will be enabled. You can safely ignore warnings related to encryption not working using Wine, this is no longer the case.
